1074c1132539b23ff61ed6483cd86bb6eec0a29b
Ashfid <ashfidable.islam@gmail.com>
2022-11-15 04:00:20 +0300

    feat: added tailwind support and colors for design

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"primary": {
+					DEFAULT: "var(--color-primary)",
+					"light": "var(--color-primary-400)"
+				}
+			}
+		},
+	},
+	plugins: [],
+}
