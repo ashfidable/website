ac957f94c756096532a615a5e582fcc4615d2b2c
Ashfid <ashfidable.islam@gmail.com>
2023-03-31 14:35:36 +0300

    feat: added preloading

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,10 +1,11 @@
 import Swup from 'swup';
 import SwupScriptsPlugin from '@swup/scripts-plugin';
 import SwupHeadPlugin from '@swup/head-plugin'
+import SwupPreloadPlugin from '@swup/preload-plugin';
 
 const swup = new Swup({
     plugins: [
-        new SwupHeadPlugin()],
+        new SwupHeadPlugin(), new SwupPreloadPlugin()],
     animationSelector: '[class*="swup-transition-"]',
     animateHistoryBrowsing: true,
 });
