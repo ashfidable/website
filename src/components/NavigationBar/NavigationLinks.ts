fe2240b940daa50a9eaf49e42a524fc558e6e900
Ashfid <ashfidable.islam@gmail.com>
2023-09-29 23:30:54 +0300

    feat: added navigation links

---
+++ src/components/NavigationBar/NavigationLinks.ts
@@ -0,0 +1,27 @@
+import {
+    Book, Bookmark,
+    ScissorsSquare, Component
+} from 'lucide-astro'
+
+export const Links = [
+    {
+        "name": "Blog",
+        "url": "/blog",
+        "icon": Book
+    },
+    {
+        "name": "Bookmarks",
+        "url": "/bookmarks",
+        "icon": Bookmark
+    },
+    {
+        "name": "Snippets",
+        "url": "/snippets",
+        "icon": ScissorsSquare
+    },
+    {
+        "name": "Uses",
+        "url": "/uses",
+        "icon": Component
+    }
+]
