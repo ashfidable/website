378128109c24aa4bb903bad8c8d79d7c6adf39b3
Ashfid <ashfidable.islam@gmail.com>
2023-10-16 18:22:01 +0300

    build: new mdx integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   site: "https://ashfidable.github.io"
 });
