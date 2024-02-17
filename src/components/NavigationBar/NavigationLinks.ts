b5fb694a2cea58683dfb668b8ad39fe4b00382e3
Ashfid <ashfidable.islam@gmail.com>
2024-02-17 17:39:49 +0300

    refactor: navigation bar support for mobile

--- src/components/NavigationBar/NavigationLinks.ts
+++ src/components/NavigationBar/NavigationLinks.ts
@@ -1,27 +1,37 @@
-import {
-    Book, Bookmark,
-    ScissorsSquare, Component
-} from 'lucide-astro'
+import { Book, Bookmark, ScissorsSquare, Component } from 'lucide-astro'
 
 export const Links = [
-    {
-        "name": "Blog",
-        "url": "/blog",
-        "icon": Book
-    },
-    {
-        "name": "Bookmarks",
-        "url": "/bookmarks",
-        "icon": Bookmark
-    },
-    {
-        "name": "Snippets",
-        "url": "/snippets",
-        "icon": ScissorsSquare
-    },
-    {
-        "name": "Uses",
-        "url": "/uses",
-        "icon": Component
-    }
+	{
+		name: 'Blog',
+		url: '/blog',
+		icon: Book
+	},
+	{
+		name: 'Bookmarks',
+		url: '/bookmarks',
+		icon: Bookmark
+	},
+	{
+		name: 'Snippets',
+		url: '/snippets',
+		icon: ScissorsSquare
+	},
+	{
+		name: 'Uses',
+		url: '/uses',
+		icon: Component
+	}
+]
+
+export const MobileLinks = [
+	{
+		name: 'Blog',
+		url: '/blog',
+		icon: Book
+	},
+	{
+		name: 'Snippets',
+		url: '/snippets',
+		icon: ScissorsSquare
+	}
 ]
