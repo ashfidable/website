64d8ec34746fa91740c1f7ddcb6fb9f2cb5d4e27
Ashfid <ashfidable.islam@gmail.com>
2024-02-26 18:07:50 +0300

    fix: tailwind.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,20 +11,6 @@ module.exports = {
 			animation: {
 				wave: 'wave-animation 2.5s linear infinite'
 			},
-			// fontSize: {
-			// 	sm: 'var(--font-size-sm)',
-			// 	base: 'var(--font-size-base)',
-			// 	lg: 'var(--font-size-lg)',
-			// 	xl: 'var(--font-size-xl)',
-			// 	'2xl': 'var(--font-size-2xl)',
-			// 	'3xl': 'var(--font-size-3xl)',
-			// 	'4xl': 'var(--font-size-4xl)',
-			// 	'5xl': 'var(--font-size-5xl)',
-			// 	'6xl': 'var(--font-size-6xl)',
-			// 	'7xl': 'var(--font-size-7xl)',
-			// 	'8xl': 'var(--font-size-8xl)',
-			// 	'9xl': 'var(--font-size-9xl)'
-			// },
 			colors: {
 				primary: {
 					DEFAULT: 'var(--color-primary)',
