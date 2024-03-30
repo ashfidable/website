908d272f5eda420f69d1a82dbd455bb779ceb3f1
Ashfid <ashfidable.islam@gmail.com>
2024-03-30 19:22:52 +0300

    feat (button): new svelte button

---
+++ src/components/button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let onclick: () => void | ((e: Event) => void) | Promise<void> | void
+</script>
+
+<button
+	class="rounded-md bg-button p-1 px-2 font-bold text-button-text-active flex gap-2 items-center font-heading tracking-wide active:scale-105 duration-100 ease-[var(--ease-custom)]"
+	on:click={onclick}
+	{...$$restProps}
+>
+	<slot />
+</button>
