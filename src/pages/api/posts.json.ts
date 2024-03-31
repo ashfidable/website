27b37943b6c4a9227dbe2fd110211b0db491de7b
Ashfid <ashfidable.islam@gmail.com>
2024-03-31 20:38:09 +0300

    refactor: formatted posts api

--- src/pages/api/posts.json.ts
+++ src/pages/api/posts.json.ts
@@ -1,16 +1,16 @@
 import type { APIRoute } from 'astro'
 import { getCollection } from 'astro:content'
-import fs from 'node:fs'
 
 const snippets = await getCollection('snippets')
 const formatSnippets = snippets.map((snippet) => {
 	return {
 		title: snippet.data.title,
-		url: `/snippets/${snippet.slug}`
+		url: `/snippets/${snippet.slug}`,
+		description: snippet.data.description,
+		slug: snippet.slug
 	}
 })
 
 export const GET: APIRoute = async ({ params, request }) => {
-	fs.writeFile('commands.json', JSON.stringify(snippets), () => console.log('Saved'))
-	return new Response(JSON.stringify(snippets))
+	return new Response(JSON.stringify(formatSnippets))
 }
