3790f804e3a0ff5d4f6990a92f384f6448465af9
Ashfid <ashfidable.islam@gmail.com>
2024-03-31 06:05:20 +0300

    style: tailwind to extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,49 +5,46 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 
 	theme: {
-		backgroundColor: {
-			background: 'var(--color-background)',
-			body: 'var(--color-background-body)',
-			card: 'var(--color-background-card)',
-			category: 'var(--color-category-background)',
-			button: 'var(--color-button-normal)',
-			'button-active': 'var(--color-button-active)',
-			'button-hover': 'var(--color-button-hover)',
-			'spotify-progress': 'var(--color-spotify-progress)',
-			'spotify-progress-outer': 'var(--color-spotify-progress-background)',
-			input: 'var(--color-input-background)',
-			blockquote: 'var(--color-blockquote-background)'
-		},
-		textColor: {
-			body: 'var(--color-body-text)',
-
-			'button-text-active': 'var(--color-button-text-active)',
-			heading: 'var(--color-heading-text)',
-			'heading-hover': 'var(--color-heading-text-hover)',
-			'datetime-text': 'var(--color-card-date-text)',
-			'icon-hover': 'var(--color-icon-hover)',
-			icon: 'var(--color-icon)',
-			'category-icon': 'var(--color-category-text)',
-			'blockquote-text': 'var(--color-blockquote-text)'
-		},
-		borderColor: {
-			highlight: 'var(--color-border-highlight)',
-			'category-highlight': 'var(--color-category-highlight)',
-			'highlight-hover': 'var(--color-border-highlight-hover)',
-			'blockquote-indicator': 'var(--color-blockquote-indicator)'
-		},
-		textDecorationColor: {
-			link: 'var(--color-link)',
-			'link-hover': 'var(--color-link-hover)'
-		},
-		ringColor: {
-			'input-normal': 'var(--color-input-ring-normal)',
-			'input-focus': 'var(--color-input-ring-focus)'
-		},
-		colors: {
-			red: colors.red[500]
-		},
 		extend: {
+			backgroundColor: {
+				background: 'var(--color-background)',
+				body: 'var(--color-background-body)',
+				card: 'var(--color-background-card)',
+				category: 'var(--color-category-background)',
+				button: 'var(--color-button-normal)',
+				'button-active': 'var(--color-button-active)',
+				'button-hover': 'var(--color-button-hover)',
+				'spotify-progress': 'var(--color-spotify-progress)',
+				'spotify-progress-outer': 'var(--color-spotify-progress-background)',
+				input: 'var(--color-input-background)',
+				blockquote: 'var(--color-blockquote-background)'
+			},
+			textColor: {
+				body: 'var(--color-body-text)',
+
+				'button-text-active': 'var(--color-button-text-active)',
+				heading: 'var(--color-heading-text)',
+				'heading-hover': 'var(--color-heading-text-hover)',
+				'datetime-text': 'var(--color-card-date-text)',
+				'icon-hover': 'var(--color-icon-hover)',
+				icon: 'var(--color-icon)',
+				'category-icon': 'var(--color-category-text)',
+				'blockquote-text': 'var(--color-blockquote-text)'
+			},
+			borderColor: {
+				highlight: 'var(--color-border-highlight)',
+				'category-highlight': 'var(--color-category-highlight)',
+				'highlight-hover': 'var(--color-border-highlight-hover)',
+				'blockquote-indicator': 'var(--color-blockquote-indicator)'
+			},
+			textDecorationColor: {
+				link: 'var(--color-link)',
+				'link-hover': 'var(--color-link-hover)'
+			},
+			ringColor: {
+				'input-normal': 'var(--color-input-ring-normal)',
+				'input-focus': 'var(--color-input-ring-focus)'
+			},
 			backgroundImage: {
 				'category-gradient': 'var(--color-category-background)'
 			},
