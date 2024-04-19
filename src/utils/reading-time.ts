d597b308b156cfa26464d50b490a2dd4d6921592
Ashfid <ashfidable.islam@gmail.com>
2024-04-20 02:07:57 +0300

    feat: add reading time

---
+++ src/utils/reading-time.ts
@@ -0,0 +1,17 @@
+import calculateReadingTime from 'reading-time'
+import { fromMarkdown } from 'mdast-util-from-markdown'
+import { toString } from 'mdast-util-to-string'
+
+export function getReadingTime(text: string) {
+	if (!text || !text.length) return undefined
+	try {
+		const { minutes } = calculateReadingTime(toString(fromMarkdown(text)))
+		if (minutes && minutes > 0) {
+			const minuteOrMinutes = minutes > 1 ? 'mins' : 'min'
+			return `${Math.ceil(minutes)} ${minuteOrMinutes} read`
+		}
+		return undefined
+	} catch (e) {
+		return undefined
+	}
+}
