91da3a73a4549aa2a180674555a74c1f8501f850
Ashfid <ashfidable.islam@gmail.com>
2024-04-21 05:32:09 +0300

    refactor: extendable button classes

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let onclick: () => void | ((e: Event) => void) | Promise<void> | void
+	export let classes = ''
 </script>
 
 <button
-	class="rounded-md bg-button p-1 px-2 font-bold text-button-text-active flex gap-2 items-center font-heading tracking-wide active:scale-105 transition-transform duration-100 ease-[var(--ease-custom)]"
+	class={`rounded-md bg-button p-1 px-2 font-semibold text-button-text-active flex gap-2 items-center font-heading tracking-wide active:scale-105 transition-transform duration-100 ease-[var(--ease-custom)] hover:outline outline-highlight outline-2 active:outline ${classes}`}
 	on:click={onclick}
 	{...$$restProps}
 >
