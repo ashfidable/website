ead18e78102da8214771f010cf5029c06de6723f
Ashfid <ashfidable.islam@gmail.com>
2024-04-23 00:25:51 +0300

    feat: new barrel file for posts atoms and implemented in snippets

---
+++ src/components/posts/atoms/index.ts
@@ -0,0 +1,23 @@
+import H1 from '$components/posts/atoms/h1.astro'
+import H2 from '$components/posts/atoms/h2.astro'
+import H3 from '$components/posts/atoms/h3.astro'
+import H4 from '$components/posts/atoms/h4.astro'
+import P from '$components/posts/atoms/p.astro'
+import BlockQuote from '$components/posts/atoms/blockquote.astro'
+import A from '$components/posts/atoms/a.astro'
+import Section from '$components/posts/atoms/section.astro'
+import OL from '$components/posts/atoms/ol.astro'
+import UL from '$components/posts/atoms/ul.astro'
+
+export const components = {
+	h1: H1,
+	h2: H2,
+	h3: H3,
+	h4: H4,
+	blockquote: BlockQuote,
+	a: A,
+	p: P,
+	section: Section,
+	ol: OL,
+	ul: UL
+}
