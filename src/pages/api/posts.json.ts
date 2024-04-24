a3ee5b28c5e71d0b4098df03c7cd1804d8db30c4
Ashfid <ashfidable.islam@gmail.com>
2024-04-24 19:22:32 +0300

    fix: merged blog and snippets in api

--- src/pages/api/posts.json.ts
+++ src/pages/api/posts.json.ts
@@ -2,6 +2,7 @@ import type { APIRoute } from 'astro'
 import { getCollection } from 'astro:content'
 
 const snippets = await getCollection('snippets')
+const blog = await getCollection('blog')
 const formatSnippets = snippets.map((snippet) => {
 	return {
 		title: snippet.data.title,
@@ -11,6 +12,17 @@ const formatSnippets = snippets.map((snippet) => {
 	}
 })
 
+const formatPosts = blog.map((post) => {
+	return {
+		title: post.data.title,
+		url: `/blog/${post.slug}`,
+		description: post.data.description,
+		slug: post.slug
+	}
+})
+
+const all = [...formatPosts, ...formatSnippets]
+
 export const GET: APIRoute = async ({ params, request }) => {
-	return new Response(JSON.stringify(formatSnippets))
+	return new Response(JSON.stringify(all))
 }
