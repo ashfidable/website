5a9d97ef5a5399ce213a0ff5fdb777d8de2be1da
Ashfid <ashfidable.islam@gmail.com>
2024-04-24 19:22:56 +0300

    refactor: wpm to 150

--- src/utils/reading-time.ts
+++ src/utils/reading-time.ts
@@ -5,7 +5,9 @@ import { toString } from 'mdast-util-to-string'
 export function getReadingTime(text: string) {
 	if (!text || !text.length) return undefined
 	try {
-		const { minutes } = calculateReadingTime(toString(fromMarkdown(text)))
+		const { minutes } = calculateReadingTime(toString(fromMarkdown(text)), {
+			wordsPerMinute: 150
+		})
 		if (minutes && minutes > 0) {
 			const minuteOrMinutes = minutes > 1 ? 'mins' : 'min'
 			return `${Math.ceil(minutes)} ${minuteOrMinutes} read`
