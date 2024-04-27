e7419224ce732576e10921b043eff3cde2c7a273
Ashfid <ashfidable.islam@gmail.com>
2024-04-27 23:52:17 +0300

    feat (rss): rss shows content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,36 +1,40 @@
 import rss from '@astrojs/rss'
 import type { APIContext } from 'astro'
-import { getCollection, type CollectionEntry } from 'astro:content'
+import { getCollection } from 'astro:content'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+
+const parser = new MarkdownIt()
 
 export async function GET(context: APIContext) {
 	const posts = await getCollection('blog')
 	const snippets = await getCollection('snippets')
 
 	return rss({
-		// `<title>` field in output xml
-		title: "Ashfid's Blog",
-		// `<description>` field in output xml
+		title: "Ashfid's Posts",
 		description:
 			'Digital Garden of Ashfid. Writing my thoughts, sharing dev secrets to simplify your coding life!',
-		// Pull in your project "site" from the endpoint context
-		// https://docs.astro.build/en/reference/api-reference/#contextsite
 		site: context.site ?? 'localhost:4321',
-		// Array of `<item>`s in output xml
-		// See "Generating items" section for examples using content collections and glob imports
 		items: [
 			...posts.map((post) => ({
 				title: post.data.title,
 				description: post.data.description,
 				tags: post.data.tags.join(', '),
 				link: `/blog/${post.slug}`,
-				pubDate: post.data.published_time
+				pubDate: post.data.published_time,
+				content: sanitizeHtml(parser.render(post.body), {
+					allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+				})
 			})),
 			...snippets.map((snippet) => ({
 				title: snippet.data.title,
 				description: snippet.data.description,
 				tags: snippet.data.tags.join(', '),
 				link: `/blog/${snippet.slug}`,
-				pubDate: snippet.data.published_time
+				pubDate: snippet.data.published_time,
+				content: sanitizeHtml(parser.render(snippet.body), {
+					allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+				})
 			}))
 		],
 		// (optional) inject custom xml
