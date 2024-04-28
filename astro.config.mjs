63a3c0a2adfae5f8c47c14e89004450cb624fe97
Ashfid <ashfidable.islam@gmail.com>
2024-04-28 04:15:56 +0300

    feat (sitemap): add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
-import svelte from '@astrojs/svelte'
-
-// https://astro.build/config
-import mdx from '@astrojs/mdx'
+import mdx from '@astrojs/mdx';
 
 // Plugins for MDX
-import expressiveCode from 'astro-expressive-code'
-import sectionize from 'remark-sectionize'
+import expressiveCode from 'astro-expressive-code';
+import sectionize from 'remark-sectionize';
 
 // astro-icon integration
-import icon from 'astro-icon'
+import icon from 'astro-icon';
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ashfid.dev',
-	markdown: {
-		remarkPlugins: [sectionize]
-	},
-	integrations: [icon(), tailwind(), svelte(), expressiveCode(), mdx()],
-	trailingSlash: 'never'
-})
+  site: 'https://ashfid.dev',
+  markdown: {
+    remarkPlugins: [sectionize]
+  },
+  integrations: [icon(), tailwind(), svelte(), expressiveCode(), mdx(), sitemap()],
+  trailingSlash: 'never'
+});
