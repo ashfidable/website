f230340224337ccc9f78a683fb927faf50ba0541
Ashfid <ashfidable.islam@gmail.com>
2024-08-08 21:17:24 +0300

    fix(command-palette): links format

--- src/pages/api/posts.json.ts
+++ src/pages/api/posts.json.ts
@@ -1,32 +1,16 @@
 import type { APIRoute } from 'astro'
 import { getCollection } from 'astro:content'
 
-const snippets = await getCollection('posts', ({ id }) => {
-	return id.startsWith('snippets/')
-})
-const blog = await getCollection('posts', ({ id }) => {
-	return id.startsWith('blog/')
-})
-const formatSnippets = snippets.map((snippet) => {
-	return {
-		title: snippet.data.title,
-		url: `/snippets/${snippet.slug}`,
-		description: snippet.data.description,
-		slug: snippet.slug
-	}
-})
-
-const formatPosts = blog.map((post) => {
+const posts = await getCollection('posts')
+const formattedPosts = posts.map((post) => {
 	return {
 		title: post.data.title,
-		url: `/blog/${post.slug}`,
+		url: post.slug,
 		description: post.data.description,
 		slug: post.slug
 	}
 })
 
-const all = [...formatPosts, ...formatSnippets]
-
 export const GET: APIRoute = async ({ params, request }) => {
-	return new Response(JSON.stringify(all))
+	return new Response(JSON.stringify(formattedPosts))
 }
